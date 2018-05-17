38642399b6e40bfdf3e42d8b8b6f1866196e3f2b
Mike Derrick <mderrick@deeplocal.com>
2018-05-17 10:33:58 -0400

    Removing the additional provider_server directory

--- provider_server/config-provider.js
+++
@@ -1,6 +0,0 @@
-var Config = {};
-
-Config.devPortSmartHome = "3000";
-Config.smartHomeProviderGoogleClientId = "ZxjqWpsYj3"; // client id that Google will use
-Config.smartHomeProvideGoogleClientSecret = "hIMH3uWlMVrqa7FAbKLBoNUMCyLCtv"; // client secret that Google will use
-Config.smartHomeProviderApiKey = "<API_KEY>"; // client API Key generated on the console
